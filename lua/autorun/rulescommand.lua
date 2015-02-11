4158be5dbae75ad52d1d5b981ded935d6c02fd6d
ZzZMarkJonesZzZ <aperturescience.cave@gmail.com>
2015-02-11 18:17:56 +0100

    Update rulescommand.lua

--- lua/autorun/rulescommand.lua
+++ lua/autorun/rulescommand.lua
@@ -10,5 +10,8 @@ if SERVER then
 end
 
 if CLIENT then
+	AddCSLuaFile("rulescommand.lua")
+	AddCSLuaFile("rulescommand/sv_rulescommand.lua")
+	AddCSLuaFile("rulescommand/cl_rulescommand.lua")
 	include("rulescommand/cl_rulesmenu.lua")
 end
